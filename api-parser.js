12f94e5869dfc783c23e92d4a03a3e7cd17e0f7b
Patricia Aas <psmaas@gmail.com>
2019-05-16 08:56:58 +0200

    Refactor

--- api-parser.js
+++ api-parser.js
@@ -1,49 +1,59 @@
+const assert = require("assert");
+
 function getLinks(document) {
     const links = document["_links"];
     const related = links["related"];
     return related;
-  }
-  
-  function getName(document) {
+}
+
+function getName(document) {
     const id = document["id"];
     const name = id["navn"];
     return name;
-  }
-  
-  function isPollingPlace(document) {
-    const id = document["id"];
-    const level = id["nivaa"];
-    return level === "stemmekrets";
-  }
-  
-  function isDistrict(document) {
-    const id = document["id"];
-    const level = id["nivaa"];
-    return level === "bydel";
-  }
-  
-  function isMunicipality(document) {
-    const id = document["id"];
-    const level = id["nivaa"];
-    return level === "kommune";
-  }
-  
-  function isCounty(document) {
-    const id = document["id"];
-    const level = id["nivaa"];
-    return level === "fylke";
-  }
-  
-  function isSamiDistrict(document) {
-    const id = document["id"];
-    const level = id["nivaa"];
-    return level === "samevalgdistrikt";
-  }
+}
 
-  exports.getLinks = getLinks;
-  exports.getName = getName;
-  exports.isPollingPlace = isPollingPlace;
-  exports.isDistrict = isDistrict;
-  exports.isMunicipality = isMunicipality;
-  exports.isCounty = isCounty;
-  exports.isSamiDistrict = isSamiDistrict;
+function getLevel(document) {
+    const id = document["id"];
+
+    if (id) {
+        assert(document.id);
+        const level = id["nivaa"];
+        return level;
+    }
+
+    assert.fail("ERROR: Could not read id from document");
+    return "Unknown";
+}
+
+function isPollingPlace(document) {
+    const level = getLevel(document);
+    return level === "stemmekrets";
+}
+
+function isDistrict(document) {
+    const level = getLevel(document);
+    return level === "bydel";
+}
+
+function isMunicipality(document) {
+    const level = getLevel(document);
+    return level === "kommune";
+}
+
+function isCounty(document) {
+    const level = getLevel(document);
+    return level === "fylke";
+}
+
+function isSamiDistrict(document) {
+    const level = getLevel(document);
+    return level === "samevalgdistrikt";
+}
+
+exports.getLinks = getLinks;
+exports.getName = getName;
+exports.isPollingPlace = isPollingPlace;
+exports.isDistrict = isDistrict;
+exports.isMunicipality = isMunicipality;
+exports.isCounty = isCounty;
+exports.isSamiDistrict = isSamiDistrict;
